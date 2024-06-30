5d15870dad03d00843c5df296a6c6ac74a56c824
okurutalphonsee <109749563+okurutalphonsee@users.noreply.github.com>
2024-06-30 17:42:11 +0300

    started analysis

---
+++ main.R
@@ -0,0 +1,27 @@
+#Load Packages
+library(pacman)
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(tidyverse,
+              plotly,
+              reactable,
+              bslib,
+              bsicons,
+              shiny,
+              gapminder,
+              rnaturalearth,
+              countrycode,
+              htmltools,
+              rgeoboundaries,
+              outbreaks,
+              here)
+
+#Summary of numbers of hours watched by Marital status
+tv_hours_table <-  
+gss_cat %>% 
+   group_by(marital) %>% 
+     summarize(mean_tv_hours = mean(tvhours,na.rm=T))
+
+write_csv(tv_hours_table,here("Tv_hours_by_marital.csv"))
+ 
+ 
+  
